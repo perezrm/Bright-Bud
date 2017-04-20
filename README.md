fc3f200f929efa33d6daafbafc26eaa09ac8acbf
Rose Mary Perez <rsmryperez@gmail.com>
2017-04-20 15:41:57 +0000

    text

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 hey
 # Bright-Bud
+#text
---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+//Hamburger-icon-animation
+$(document).ready(function() {
+	$('.navbar-toggle').click(function() {
+    $(this).toggleClass('open');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
